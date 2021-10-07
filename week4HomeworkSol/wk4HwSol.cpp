c421da2490dcc613d795f9dec4c874f4197c00b1
Mohammad1160TA <zemoha10@gmail.com>
2021-10-06 19:20:53 -0700

    week4 HomeWork Sol

--- week4HomeworkSol/wk4HwSol.cpp
+++ week4HomeworkSol/wk4HwSol.cpp
@@ -3,15 +3,11 @@
 using namespace std;
 
 void display_array(int* array, int size) {
+    cout << " [ ";
     for (int i = 0; i < size; i++) {
-        if (i == 0) {
-            cout << " [ " << array[i] << ",";
-        } else if (i == size - 1) {
-            cout << array[i] << " ]";
-        } else {
-            cout << array[i] << ",";
-        }
+        cout << array[i] << ",";
     }
+    cout << "]";
 }
 
 /*1. 
